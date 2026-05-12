fd5cc89d67b4725a5cd6bd01213bc2835494dff1
M-I-C-H-A-E-L-C <michaelc1242@nycstudents.net>
2026-05-12 10:16:44 -0400

    1

--- HorseRacing.py
+++ HorseRacing.py
@@ -9,9 +9,15 @@ while accuracy == "no":
     print("You have inputed", betval, "dollars, is that correct?")
     accuracy = input("").lower()
 mode = input("'top 3 horses' or 'big winner' ").lower()
-time.sleep(1)
+time.sleep(0.5)
 if mode == "top 3 horses":
-    horseval3 = int(input("8 Horses! Choose 3! Choose Wisely! Ex: 1 2 3"))
+    horseval3 = int(input("8 Horses! Choose 3! Choose Wisely! Ex: 123: "))
+    time.sleep(0.5)
+    print("You have inputed horses", horseval3, ", is that correct?")
+    accuracy1 = input("").lower()
 elif mode == "big winner":
     print("ex: 1")
-    horseval1 = int(input("8 Horses! Choose 3! Choose Wisely! Ex: 1"))
+    horseval1 = int(input("8 Horses! Choose 1! Choose Wisely! Ex: 1: "))
+    time.sleep(0.5)
+    print("You have inputed horse", horseval1, ", is that correct?")
+    accuracy1 = input("").lower()
