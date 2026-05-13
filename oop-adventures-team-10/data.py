ce689d6503aa71eee8a95e51e3e3decd8d2c316d
QC2222 <quintonc12@nycstudents.net>
2026-05-13 10:17:09 -0400

    1

--- oop-adventures-team-10/data.py
+++ oop-adventures-team-10/data.py
@@ -1,8 +1,6 @@
-import time
 import json
-import random
-
-difficulties = json.load(open("oop-adventures-team-10./difficulties.json", encoding="utf8"))
+diff = open("./difficulties.json", encoding="utf8")
+difficulties = json.load(diff)
 
 class character:
     def __init__(self, debt, interest, payments, money, hunger, time):
@@ -34,34 +32,3 @@ class character:
         self.time = 6
         self.debt -= difficulties[self.x]["daily payment"]
         self.debt *= difficulties[self.x]["weekly interest"]
-
-
-
-difficulties = json.load(open("oop-adventures-team-10./difficulties.json", encoding="utf8"))
-currentdiff = int(10)
-trueorfalse = 0
-def wipescreen(x):
-    for i in range(x):
-        print("")
-
-input("You've been slacking. You've racked up millions of debt, and you need to pay it back in full. Otherwise, it'll be quite unfortunate.")
-wipescreen(100)
-for parts in difficulties:
-    print(parts["difficulty"], ": Start with a debt of $", parts["starting debt"])
-currentdiff = input("Choose a difficulty: ").lower()
-while trueorfalse != True:
-    for parts in difficulties:
-        if currentdiff == parts["difficulty"].lower() or currentdiff == parts["number"]:
-            currentdiff = parts["number"]
-            trueorfalse = 1
-            break
-    if trueorfalse != True:
-        currentdiff = input("Choose a valid difficulty: ").lower()
-input("What is your name")
-player = character(difficulties[currentdiff]["starting debt"], difficulties[currentdiff]["weekly interest"], difficulties[currentdiff]["daily payment"], 50000, 100, 6)
-
-while player.money >= 0 and player.hunger >= 0:
-    wipescreen(50)
-    player.terminal()
-    player.activities()
-    action = input()
