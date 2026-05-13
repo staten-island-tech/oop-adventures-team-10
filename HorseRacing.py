69652c32c3b6980460d19acc6618c7af4075d41f
M-I-C-H-A-E-L-C <michaelc1242@nycstudents.net>
2026-05-13 10:17:42 -0400

    05/13/26

--- HorseRacing.py
+++ HorseRacing.py
@@ -1,6 +1,7 @@
 import random
 import time
-print("Welcome to the amazing world of Horse Racing!")
+race = True
+print("Welcome to the el horsey where u lose all ur money, but theres a chance you can make 5 buckaroos!")
 time.sleep(1)
 accuracy = "no"
 while accuracy == "no":
@@ -21,3 +22,16 @@ elif mode == "big winner":
     time.sleep(0.5)
     print("You have inputed horse", horseval1, ", is that correct?")
     accuracy1 = input("").lower()
+while race == True:
+    h1 = random.randint(1,8)
+    time.sleep(random.randint(1, 2))
+    h2 = random.randint(1,8)
+    time.sleep(random.randint(1, 2))
+    h3 = random.randint(1,8)
+    while h1 == h2:
+        h2 = random.randint(1,8)
+    while h3 == h2 or h3 == h1:
+        h3 = random.randint(1,8)
+    print(h1)
+    print(h2)
+    print(h3)
