1506a994b245712dc4e63c0dc5dfe9fc5eac89e4
M-I-C-H-A-E-L-C <michaelc1242@nycstudents.net>
2026-05-20 10:17:52 -0400

    05/20

--- gambling.py
+++ gambling.py
@@ -9,13 +9,17 @@ import requests
 #Spades (\(\spades \)): A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K
 
 name = "m"
-url = "https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2"
+url = "https://deckofcardsapi.com/api/deck/new/"
 response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
     print(data)
 else:
     print(f"Failed to fetch data. Status code: {response.status_code}")
+draw = input("Would you like to draw a card?").lower()
+    
+if draw == "yes":
 
+    
 print("Good afternoon", name, "welcome to blackjack...")
 
