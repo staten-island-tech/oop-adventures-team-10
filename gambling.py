963bd51b96100d68c7e471a5df0da0574c55193b
M-I-C-H-A-E-L-C <michaelc1242@nycstudents.net>
2026-05-29 10:01:22 -0400

    05/29

--- gambling.py
+++ gambling.py
@@ -1,6 +1,6 @@
 import random
 import requests
-
+import time
 # BlackJack
 val = 0
 val1 = 0
@@ -22,6 +22,8 @@ cards = getCards()
 
 # Your Cards
 
+print("_________________________Your cards________________________")
+
 print("The dealer has dealed you your cards...")
 for i in range(2):
     url = "https://deckofcardsapi.com/api/deck/new/draw/?count=21"
@@ -34,9 +36,12 @@ for i in range(2):
         x = 10
 
     elif x == "ACE":
-        x = 1
+        if val <= 11:
+            x = 10
+        elif val >= 12:
+            x = 1
     val += int(x)
-print(val)
+print("🔵 your value", val)
 
 # Do you want to hit or stand?
 
@@ -45,59 +50,94 @@ while val <= 21:
 
     if ht_stnd == "HIT":
         response = requests.get(url)
-        z = (cards["cards"][c]["value"]).upper()
-        v = (cards["cards"][c]["suit"]).upper()
-        print(z,"of", v)
-        if z == "JACK" or z == "KING" or z == "QUEEN":
-            z = 10
-        elif z == "ACE":
-            z = 1
-        val += int(z)
+        x = (cards["cards"][c]["value"]).upper()
+        y = (cards["cards"][c]["suit"]).upper()
+        print(x,"of", y)
+        if x == "JACK" or x == "KING" or x == "QUEEN":
+            x = 10
+        elif x == "ACE":
+            if val <= 11:
+                x = 10
+            elif val >= 12:
+                x = 1
+        val += int(x)
         c += 1
-
+        print("🔵 your value", val)
+        time.sleep(0.5)
     elif ht_stnd == "STAND":
         break
 
     else:
         print("It's hit or stand buddy")
 
-    print(val)
+print("🔵 your final value", val)
+
+# Your Outcome
 
 if val >= 22:
     print("You busted everywhere")
 
-else:
-    print("final value:", val)
-
 # Dealers Deck
 
-print("Your dealers deck")
-for i in range(2):
-    response = requests.get(url)
-    x = (cards["cards"][c]["value"]).upper()
-    y = (cards["cards"][c]["suit"]).upper()
-    print(x,"of", y)
-    if x == "JACK" or x == "KING" or x == "QUEEN":
-        x = 10
-    elif x == "ACE":
-        x = 1
-    val1 += int(x)
-    c += 1
-print(val1)
+print("______________________Dealer's cards______________________")
 
-# Dealer Hits or Stands
+if val <= 21:
+    print("Your dealers deck")
+    for i in range(2):
+        response = requests.get(url)
+        x = (cards["cards"][c]["value"]).upper()
+        y = (cards["cards"][c]["suit"]).upper()
+        print(x,"of", y)
+        if x == "JACK" or x == "KING" or x == "QUEEN":
+            x = 10
+        elif x == "ACE":
+            if val1 <= 11:
+                x = 10
+            elif val1 >= 12:
+                x = 1
+        val1 += int(x)
+        c += 1
+    print("🔴 dealers value", val1)
+    time.sleep(0.5)
+    # Dealer Hits or Stands
 
-while val1 <= 16:
-    print("Dealer Hits")
-    response = requests.get(url)
-    x = (cards["cards"][c]["value"]).upper()
-    y = (cards["cards"][c]["suit"]).upper()
-    print(x,"of", y)
+    while val1 <= 16:
+        print("Dealer Hits")
+        response = requests.get(url)
+        x = (cards["cards"][c]["value"]).upper()
+        y = (cards["cards"][c]["suit"]).upper()
+        print(x,"of", y)
 
-    if x == "JACK" or x == "KING" or x == "QUEEN":
-        x = 10
+        if x == "JACK" or x == "KING" or x == "QUEEN":
+            x = 10
 
-    elif x == "ACE":
-        x = 1
-    val1 += int(x)
-    c += 1
+        elif x == "ACE":
+            if val1 <= 11:
+                x = 10
+            elif val1 >= 12:
+                x = 1
+        val1 += int(x)
+        c += 1
+        print("🔴 dealers value", val1)
+        time.sleep(0.5)
+    print("🔴 dealers final value", val1)
+
+    # Dealer Outcome
+
+    if val1 >= 22:
+        print("Dealer busted everywhere")
+
+    # Game outcome
+
+    if val1 <= 21:
+        if val > val1:
+            print("You Won!!")
+        elif val == val1:
+            print("It's a tie!")
+        else:
+            print("You lostt")
+
+else:
+    print("You lostt")
+
+
